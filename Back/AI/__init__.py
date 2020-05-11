9eadde648f849a80e4adb7b1085920eaf1fa7827
leek <leek018@naver.com>
2020-05-11 13:00:24 +0900

    project frame

--- Back/AI/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from . import dataload
-_all_=['dataload']
-__version__='0.1.0'
--- Back/Django/main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-# from django.contrib.auth import get_user_model
-# # Register your models here.
-# admin.site.register(get_user_model())
-admin.site.register(Story)
