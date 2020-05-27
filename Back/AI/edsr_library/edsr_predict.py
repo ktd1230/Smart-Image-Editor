e61bd631643530ebb079bb5d91f594043ddfd1b4
Kim Daedeul <ksh021144@naver.com>
2020-05-27 12:03:46 +0900

    S02P31C101-37 [+] Add edsr_predict
    포함되어야 하는 파일이 많아서 별도의 디렉토리에 작성했으며, 디렉토리 구조는 차후에 다시 수정될 여지가 있음
    
    
    Former-commit-id: c58acbff2702ebaec70287102fc695425225375a

---
+++ Back/AI/edsr_library/edsr_predict.py
@@ -0,0 +1,44 @@
+import torch
+# import config
+
+import sys
+import torch
+
+import utility
+import data
+import model
+import loss
+from option import args
+from option import set_setting_value_edsr
+from trainer import Trainer
+import timeit
+
+
+def predict(images="", root_path="", AI_directory_path="", model_type="EDSR"):
+    """
+    :param images: image의 이름 (특화 프로젝트 때, 복수의 이미지 파일을 받아서 images로 명명됨)
+    :param root_path: image가 저장된 디렉토리
+    :param AI_directory_path: 모델이 저장된 디렉토리
+    :param model_type:
+    :return: 생성된 이미지 파일 경로+이름 (list)
+    """
+
+    if model_type == "EDSR":
+        set_setting_value_edsr(images, root_path)
+        torch.manual_seed(args.seed)
+        checkpoint = utility.checkpoint(args)
+        if checkpoint.ok:
+            loader = data.Data(args)
+            _model = model.Model(args, checkpoint)
+            _loss = loss.Loss(args, checkpoint) if not args.test_only else None
+            t = Trainer(args, loader, _model, _loss, checkpoint)
+            result = t.test()  # return value is saved image path(and image name). list type.
+            checkpoint.done()
+            return result  # list
+
+def main():
+    predict()
+
+
+if __name__ == "__main__":
+    main()
