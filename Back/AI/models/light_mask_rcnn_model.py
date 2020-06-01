fdf16e7615c8014397020bfdf2318170c76695f6
leek <leek018@naver.com>
2020-06-01 10:26:35 +0900

    S02P31C101-49 [+UPDATE] Mask rcnn training code
    Former-commit-id: f52f550af7688ad20e55ad8605e82eec978551e2

--- Back/AI/models/light_mask_rcnn_model.py
+++ Back/AI/models/light_mask_rcnn_model.py
@@ -21,5 +21,44 @@ def get_mask_rcnn_model(layers,num_classes,out_channels=256,cfg=None):
     model = models.detection.MaskRCNN(backbone, num_classes)
     return model
 
+def get_pruned_config(custom_mrcnn_model,percent):
+    total_size = 0
+    bn = []
+    for m in custom_mrcnn_model.parameters():
+        if isinstance(m,nn.BatchNorm2d):
+            bn.append(m)
+            total_size += m.weight.data.shape[0]
+
+    candidate_for_bn = torch.zeros(total_size)
+    index = 0
+    for m in bn:
+        size = m.weight.data.shape[0]
+        candidate_for_bn[index:(index+size)] = m.weight.data.abs().clone()
+        index += size
+    y,i = torch.sort(candidate_for_bn)
+    threshold_index = int(total_size * percent)
+    threshold = y[threshold_index]
+
+    pruned = 0
+    pruned_cfg = []
+    for m in bn:
+        weight_copy = m.weight.data.clone()
+        mask = weight_copy.abs().gt(threshold).float()
+        pruned = pruned + mask.shape[0] - torch.sum(mask)
+        m.weight.data.mul_(mask)
+        m.bias.data.mul_(mask)
+        pruned_cfg.append(int(torch.sum(mask)))
+
+    before_cfg = custom_mrcnn_model.cfg
+    new_cfg = []
+    index = 0
+    for cfg_list in before_cfg:
+        size = len(cfg_list)
+        new_cfg.append([ele for ele in pruned_cfg[index:index+size]])
+        index +=size
+    return new_cfg
+
+
+
 
 
