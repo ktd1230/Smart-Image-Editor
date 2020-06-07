ee7b106e962829cf265846d72bf6e202f7e29d54
김대들 <ksh021144@naver.com>
2020-06-07 14:51:03 +0000

    S02P31C101-71 [+] release uwsgi
    Former-commit-id: 9d113b2987c704c2aca9cba7644ba992861146ec [formerly dd472480b77bcabde90792e19665a3e966c17963] [formerly dd472480b77bcabde90792e19665a3e966c17963 [formerly 4a12dc4b00e2daa7ccdb9c787e8c8977a42cbd0f]]
    Former-commit-id: 9ef9b41546fd422bc4e533f803811171e108ae5a
    Former-commit-id: 7fa98ad7c9b76d7383b9550e231e9d1928e7b4fd

--- Front/src/axios_common.js
+++ Front/src/axios_common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:8000",  
-	baseURL: "http://k02c1011.p.ssafy.io:8000",
+  baseURL: "http://k02c1011.p.ssafy.io:8000",
   headers: {
     "Content-type": "application/json",
   }
